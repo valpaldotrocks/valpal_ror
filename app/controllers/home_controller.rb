2e90681bff3bcf771c92c441eb998bf8ac7678a1
Greg Dziemidowicz <greg@cogent.co>
2015-01-31 17:55:58 +1100

    only go to sandbox if we ask you too

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @paypal_endpoint = params[:sandbox].present? ? "www.sandbox.paypal.com" : "www.paypal.com" 
     if params[:id]
       @pal = Pal.find(params[:id]) 
     else
