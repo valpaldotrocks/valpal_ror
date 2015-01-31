c536f14d0d08be9b2f0846bb777326b712e4bab4
Greg Dziemidowicz <greg@cogent.co>
2015-02-01 10:53:05 +1100

    actual profile picture

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,12 @@ class HomeController < ApplicationController
     @pals = Pal.where("latitude is NOT NULL and longitude is NOT NULL").all
   end
 
+  private
+
+  helper do
+    def pal_data(pal)
+      {name: pal.name, lat: pal.latitude, long: pal.longitude, url: pal.avatar.url }
+    end
+  end
+
 end
