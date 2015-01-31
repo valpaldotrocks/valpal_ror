010a8c3753c854aabc77f3927f07fc87734896cd
Greg Dziemidowicz <greg@cogent.co>
2015-01-31 17:10:58 +1100

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Pal.create(name: "melb1", email: "greg@cogent.co")
+Pal.create(name: "melb2", email: "greg@cogent.co")
+Pal.create(name: "melb3", email: "greg@cogent.co")
+Pal.create(name: "melb4", email: "greg@cogent.co")
+Pal.create(name: "melb5", email: "greg@cogent.co")
+Pal.create(name: "melb6", email: "greg@cogent.co")
+Pal.create(name: "melb7", email: "greg@cogent.co")
+Pal.create(name: "melb8", email: "greg@cogent.co")
+Pal.create(name: "melb9", email: "greg@cogent.co")
+Pal.create(name: "melb10", email: "greg@cogent.co")
